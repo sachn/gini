4590d53d0ac223fd2ce120a0870cd44690a9dca7
sachin.khadse <sachin.khadse@accenture.com>
2018-03-20 20:27:41 +0100

    develop-WishType-display: Enable Spring Boot
    - Enable JPA
    - Add connection properties on persistence.xml
    - Create Spring Boot Application
    - Adjust WishController to generate static response

---
+++ src/main/java/com/nova/gini/Application.java
@@ -0,0 +1,17 @@
+package com.nova.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main application
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/com/nova/gini/controller/wish/WishController.java
+++ src/main/java/com/nova/gini/controller/wish/WishController.java
@@ -1,25 +1,63 @@
 package com.nova.gini.controller.wish;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nova.gini.wish.WishTypeService;
 import com.nova.gini.wish.domain.WishType;
 
 @RestController
 @RequestMapping("/rest")
 public class WishController {
 
-    @Autowired
-    private WishTypeService wishTypeService;
+    // @Autowired
+    // private WishTypeService wishTypeService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/wishtype")
-    public List<WishType> getAllWishTypes() {
+    private static Long nextId;
 
-        return wishTypeService.getAll();
+    private static Map<Long, WishType> wishTypes;
+
+    private static WishType save(WishType wishType) {
+
+        if (wishTypes == null) {
+            wishTypes = new HashMap<Long, WishType>();
+            nextId = 1L;
+        }
+        wishType.setId(nextId);
+        wishTypes.put(wishType.getId(), wishType);
+        nextId = nextId + 1;
+
+        return wishType;
+    }
+
+    static {
+        WishType financialWishType = new WishType();
+        financialWishType.setName("Financial");
+        financialWishType.setDescription("Financial Wish");
+        save(financialWishType);
+
+        WishType skillWishType = new WishType();
+        skillWishType.setName("Skill");
+        skillWishType.setDescription("Skill Wish");
+        save(skillWishType);
+    }
+
+    @RequestMapping(value = "/wishtype", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<WishType>> getAllWishTypes() {
+
+        // Collection<WishType> wishTypes = wishTypeService.getAll();
+        //
+        // return new ResponseEntity<Collection<WishType>>(wishTypes, HttpStatus.OK);
+
+        Collection<WishType> responseWishTypes = wishTypes.values();
+
+        return new ResponseEntity<Collection<WishType>>(responseWishTypes, HttpStatus.OK);
     }
 }
